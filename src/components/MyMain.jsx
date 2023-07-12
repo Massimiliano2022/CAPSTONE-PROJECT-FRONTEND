e4cbd69f0872f360da5ed3c12c1a6a948a215c49
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-07-13 01:25:29 +0200

    fix MyMain se utente loggato

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -1,9 +1,9 @@
 import mainImg from '../img/main.svg'
 
 import { BiLogIn } from "react-icons/bi";
-import { BsFillPersonFill } from "react-icons/bs";
-import { Link } from 'react-router-dom';
-import { Col, Container, Row } from "react-bootstrap";
+import { BsFillPersonFill, BsCurrencyExchange,BsWallet2} from "react-icons/bs";
+import { Link, useNavigate } from 'react-router-dom';
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { getCurrentCryptoData } from '../redux/actions'
@@ -11,6 +11,8 @@ import MyCryptoCard from './MyCryptoCard'
 
 
 const MyMain = () => {
+
+    const navigator = useNavigate();
 
     const utenteCorrente = useSelector(state => state.utenteCorrente.userData);
 
@@ -20,6 +22,12 @@ const MyMain = () => {
         dispatch(getCurrentCryptoData());
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    const effettuaOperazione = () => {
+        const randomIndex = Math.floor(Math.random() * cryptosPrice.length);
+        const selectedCrypto = cryptosPrice[randomIndex];
+        navigator(`/crypto/${selectedCrypto.simbolo.toLowerCase()}`);
+    }
 
     return (
         <>
@@ -53,12 +61,39 @@ const MyMain = () => {
                         <img className="object-fit-cover img-fluid" src={mainImg} alt='exchange' style={{ maxHeight: "325px" }} />
                     </Col>
                 </Row>
+                <Row className={`d-flex py-5 ${utenteCorrente ? '' : 'd-none'}`}>
+                    <Col sm={6} className="d-flex flex-column">
+                        <h1>Compra e scambia Crypto su DigitFin</h1>
+                        <Button
+                            onClick={effettuaOperazione}
+                            style={{ color: "black" }}
+                            className="btn btn-warning nav-link text-center rounded rounded-1 p-2 w-50 mt-4"
+                        >
+                            <span className='fs-4 pe-2'>
+                                <BsCurrencyExchange />
+                            </span>
+                            Compra
+                        </Button>
+                        <Link
+                            to={"/wallet"}
+                            className="btn btn-dark nav-link text-center rounded rounded-1 p-2 w-50 mt-4"
+                        >
+                            <span className='fs-4 pe-2'>
+                                <BsWallet2 />
+                            </span>
+                            Wallet
+                        </Link>
+                    </Col>
+                    <Col sm={6} className="d-flex justify-content-end">
+                        <img className="object-fit-cover img-fluid" src={mainImg} alt='exchange' style={{ maxHeight: "325px" }} />
+                    </Col>
+                </Row>
             </Container>
             <Container fluid className="text-light px-5" style={{ background: "#1E1E1E" }}>
                 <Row className='py-5'>
                     <>
                         {cryptosPrice.slice(0, 4).map(crypto => (
-                            <MyCryptoCard crypto={crypto} key={crypto.id}/>
+                            <MyCryptoCard crypto={crypto} key={crypto.id} />
                         ))}
                     </>
                 </Row>
