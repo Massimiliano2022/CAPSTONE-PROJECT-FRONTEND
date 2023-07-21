d832db96f24201380762a2f6a9364f27af48a9ba
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-07-21 18:33:41 +0200

    Fix responsive in MyCryptoList.jsx

--- src/components/MyCryptoCard.jsx
+++ src/components/MyCryptoCard.jsx
@@ -27,33 +27,35 @@ const MyCryptoCard = ({ crypto }) => {
     const variazioneColor = crypto.percententuale_variazione_1h < 0 ? "#E31903" : "#0FC67E";
 
     return (
-        <Col md={12} lg={6} className='mb-4'>
-            <Link to={`/crypto/${cryptoSymbol}`} className="nav-link">
-                <Card className='' style={{ background: "#2d2d2d" }}>
-                    <Card.Body className='px-4 py-3 fs-6'>
+        <Col md={6} className='mb-4'>
+            <Card className='h-100' style={{ background: "#2d2d2d", minHeight: "250px" }}>
+                <Link to={`/crypto/${cryptoSymbol}`} className="nav-link h-100">
+                    <Card.Body className='px-4 py-3 fs-6 h-100 d-flex flex-column justify-content-between'>
                         <img src={cryptoLogos[cryptoSymbol]} alt='Bitcoin Logo' width={50} className="img-fluid object-fit-cover" />
                         <div className='d-flex justify-content-between py-4'>
                             <Card.Text className='m-0 w-33.3'>{crypto.simbolo}/USDT</Card.Text>
                             <Card.Text className='m-0 w-33.3'>{crypto.prezzo.toFixed(4)} $</Card.Text>
                             <Card.Text className='m-0 w-33.3' style={{ color: variazioneColor }}>{crypto.percententuale_variazione_1h}</Card.Text>
                         </div>
-                        <Button
-                            type="button"
-                            variant="success"
-                            className="text-light rounded rounded-1 me-3"
-                        >
-                            Compra
-                        </Button>
-                        <Button
-                            type="button"
-                            variant="danger"
-                            className="text-light rounded rounded-1"
-                        >
-                            Vendi
-                        </Button>
+                        <div className='d-flex justify-content-start'>
+                            <Button
+                                type="button"
+                                variant="success"
+                                className="text-light rounded rounded-1 me-3"
+                            >
+                                Compra
+                            </Button>
+                            <Button
+                                type="button"
+                                variant="danger"
+                                className="text-light rounded rounded-1"
+                            >
+                                Vendi
+                            </Button>
+                        </div>
                     </Card.Body>
-                </Card>
-            </Link>
+                </Link>
+            </Card>
         </Col>
     );
 }
