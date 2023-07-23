a38fce91f3ccf03cd8e419b5e9e2197ccfac0fb1
Massimiliano2022 <120369719+Massimiliano2022@users.noreply.github.com>
2023-07-23 22:06:52 +0200

    fix font-size footer

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -7,44 +7,44 @@ const MyFooter = () =>{
         <Container fluid className="d-none d-md-block py-5" style={{background: "#2d2d2d"}}>
             <Row className="d-flex text-light px-5">
                 <Col className="p-0">
-                    <p className="fs-6 mb-1">Chi siamo</p>
-                    <p className="fs-6 m-0 text-muted">Informazioni</p>
-                    <p className="fs-6 m-0 text-muted">Privacy</p>
-                    <p className="fs-6 m-0 text-muted">Avvisi</p>
-                    <p className="fs-6 m-0 text-muted">Annunci</p>
-                    <p className="fs-6 m-0 text-muted">Notizie</p>
+                    <p className="fs-5 mb-1">Chi siamo</p>
+                    <p className="fs-5 m-0 text-muted">Informazioni</p>
+                    <p className="fs-5 m-0 text-muted">Privacy</p>
+                    <p className="fs-5 m-0 text-muted">Avvisi</p>
+                    <p className="fs-5 m-0 text-muted">Annunci</p>
+                    <p className="fs-5 m-0 text-muted">Notizie</p>
                 </Col>
                 <Col className="p-0">
-                    <p className="fs-6 mb-1">Prodotti</p>
-                    <p className="fs-6 m-0 text-muted">Exchange</p>
-                    <p className="fs-6 m-0 text-muted">Accademy</p>
-                    <p className="fs-6 m-0 text-muted">NFT</p>
-                    <p className="fs-6 m-0 text-muted">Report fiscale</p>
+                    <p className="fs-5 mb-1">Prodotti</p>
+                    <p className="fs-5 m-0 text-muted">Exchange</p>
+                    <p className="fs-5 m-0 text-muted">Accademy</p>
+                    <p className="fs-5 m-0 text-muted">NFT</p>
+                    <p className="fs-5 m-0 text-muted">Report fiscale</p>
                 </Col>
                 <Col className="p-0">
-                    <p className="fs-6 mb-1">Servizio</p>
-                    <p className="fs-6 m-0 text-muted">Download</p>
-                    <p className="fs-6 m-0 text-muted">Applicazione desktop</p>
-                    <p className="fs-6 m-0 text-muted">Acquista criptovalute</p>
-                    <p className="fs-6 m-0 text-muted">Referral</p>
-                    <p className="fs-6 m-0 text-muted">Affiliate</p>
+                    <p className="fs-5 mb-1">Servizio</p>
+                    <p className="fs-5 m-0 text-muted">Download</p>
+                    <p className="fs-5 m-0 text-muted">Applicazione desktop</p>
+                    <p className="fs-5 m-0 text-muted">Acquista criptovalute</p>
+                    <p className="fs-5 m-0 text-muted">Referral</p>
+                    <p className="fs-5 m-0 text-muted">Affiliate</p>
                 </Col>
                 <Col className="p-0">
-                    <p className="fs-6 mb-1">Supporto</p>
-                    <p className="fs-6 m-0 text-muted">Centro di supporto</p>
-                    <p className="fs-6 m-0 text-muted">Chat di supporto</p>
-                    <p className="fs-6 m-0 text-muted">Commissioni</p>
-                    <p className="fs-6 m-0 text-muted">Regole di trading</p>
+                    <p className="fs-5 mb-1">Supporto</p>
+                    <p className="fs-5 m-0 text-muted">Centro di supporto</p>
+                    <p className="fs-5 m-0 text-muted">Chat di supporto</p>
+                    <p className="fs-5 m-0 text-muted">Commissioni</p>
+                    <p className="fs-5 m-0 text-muted">Regole di trading</p>
                 </Col>
                 <Col className="p-0">
-                    <p className="fs-6 mb-1">Apprendi</p>
-                    <p className="fs-6 m-0 text-muted">Learn & Earn</p>
-                    <p className="fs-6 m-0 text-muted">Esplora i prezzi crypto</p>
-                    <p className="fs-6 m-0 text-muted">Prezzo Bitcoin</p>
-                    <p className="fs-6 m-0 text-muted">Prezzo Ethereum</p>
+                    <p className="fs-5 mb-1">Apprendi</p>
+                    <p className="fs-5 m-0 text-muted">Learn & Earn</p>
+                    <p className="fs-5 m-0 text-muted">Esplora i prezzi crypto</p>
+                    <p className="fs-5 m-0 text-muted">Prezzo Bitcoin</p>
+                    <p className="fs-5 m-0 text-muted">Prezzo Ethereum</p>
                 </Col>
                 <Col className="p-0">
-                    <p className="fs-6 mb-1">Community</p>
+                    <p className="fs-5 mb-1">Community</p>
                     <div className="d-flex flex-row mt-2">
                         <BsDiscord className="fs-5 me-2 text-muted"/>
                         <BsTelegram className="fs-5 me-2 text-muted"/>
